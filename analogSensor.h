b95e52e9421fb91b67625568378a1711fd4a652e
strix190788 <strix190788@gmail.com>
2026-03-21 11:27:08 +0400

    Initial commit

---
+++ analogSensor.h
@@ -0,0 +1,27 @@
+class analogSensor {
+public:
+  analogSensor(byte Sensorpin, byte pinV = 0, byte pinGND = 0) {
+    _pin = Sensorpin;
+    if (pinV != 0) {
+      pinMode(pinV, OUTPUT);
+      digitalWrite(pinV, 1);
+    }
+    if (pinGND != 0) {
+      pinMode(pinGND, OUTPUT);
+      digitalWrite(pinGND, 0);
+    }
+  }
+  int read() {
+	_value = analogRead(_pin);
+    return _value;
+  }
+  
+  int mapRead() {
+    return map(analogRead(_pin), 0, 1023, 0, 100);
+  }
+  
+private:
+  byte _pin;
+  int _value;
+  
+};
